912a6a7c2c7b5cff694a02eea7951bdc92b77f65
laptop <harkmollis@gmail.com>
2018-10-04 00:43:53 -0500

    auth0 gmail only

--- ClientApp/Services/Auth/Auth.tsx
+++ ClientApp/Services/Auth/Auth.tsx
@@ -2,9 +2,9 @@
 
 export default class Auth {
 	auth0 = new auth0.WebAuth({
-		domain: 'markhollis.auth0.com',
-		clientID: 'CGGC-nBID0jhzDmJPgYrmmbkoMKnxmfb',
-		redirectUri: 'http://localhost:63087/callback',
+		domain: 'taproleague.auth0.com',
+		clientID: 'Dy0MGualzLv8MUr3nQH5Q8m7immo0WFp',
+		redirectUri: 'http://localhost:57171/callback',
 		responseType: 'token id_token',
 		scope: 'openid'
 	});
